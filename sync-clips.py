ee3d0a92a9eee8ebc384d1489ad82c838e772a5a
Mikolaj98p <20612773+Mikolaj98p@users.noreply.github.com>
2025-02-02 09:14:56 +0000

    added infinite option

--- sync-clips.py
+++ sync-clips.py
@@ -118,6 +118,10 @@ if __name__ == '__main__':
 
         url_config = copy.deepcopy(config['configs'].get(url_config_name))
 
+        if 'retries' in url_config and url_config['retries'] == 'infinite':
+          url_config['retries'] = float('inf')
+          pass
+
         download(url_config, url)
 
         if url.get('disable', 'false') == 'afterDownload' or url_config.get('disableAfterDownload', False):
